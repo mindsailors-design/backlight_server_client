7ee6358d24508575bccc922ad80196c29ce84839
Jacek Skarupa <jacek.skarupa@mindsailors.com>
2025-04-10 13:09:50 +0100

    improved readability, added endpoints urls as constants

--- backlight_client.py
+++ backlight_client.py
@@ -3,157 +3,145 @@ import requests
 
 MAIN_URL = "http://127.0.0.1:8000"
 
+SET_BRIGHTNESS_URL      = "/set_brightness"
+SET_COLOR_TEMP_URL      = "/set_color_temp"
+INCREASE_BRIGHTNESS_URL = "/increase_brightness"
+DECREASE_BRIGHTNESS_URL = "/decrease_brightness"
+INCREASE_COLOR_TEMP_URL = "/increase_color_temp"
+DECREASE_COLOR_TEMP_URL = "/decrease_color_temp"
+GET_BRIGHTNRESS_URL     = "/get_brightness"
+GET_COLOR_TEMP_URL      = "/get_color_temp"
+
+COMMON_HEADER = {"accept": "application/json"}
+
 app = typer.Typer()
 
 @app.command()
 def set_brightness(brightness: float):
-    print(f"Setting brightness, {brightness}")
     """
     curl -X 'POST' \
     'http://127.0.0.1:8000/set_brightness?data=1' \
     -H 'accept: application/json' \
     -d ''
     """
+    print(f"Setting brightness: {brightness}")
 
-    set_brightness_url = "/set_brightness"
-    url = MAIN_URL + set_brightness_url
+    url = MAIN_URL + SET_BRIGHTNESS_URL
     params = {"data": brightness}
-    headers = {"accept": "application/json"}
 
-    response = requests.post(url, headers=headers, params=params, data="")
+    response = requests.post(url, headers=COMMON_HEADER, params=params)
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def set_color_temp(color_temp: float):
-    print(f"Setting color temperature: {color_temp}")
     """
     curl -X 'POST' \
     'http://127.0.0.1:8000/set_color_temp?data=1' \
     -H 'accept: application/json' \
     -d ''
     """
+    print(f"Setting color temperature: {color_temp}")
 
-    set_color_temp_url = "/set_color_temp"
-    url = MAIN_URL + set_color_temp_url
+    url = MAIN_URL + SET_COLOR_TEMP_URL
     params = {"data": color_temp}
-    headers = {"accept": "application/json"}
 
-    response = requests.post(url, headers=headers, params=params, data = "")
+    response = requests.post(url, headers=COMMON_HEADER, params=params)
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def increase_brightness():
-    print("Increasing brightness")
     """
     curl -X 'POST' \
     'http://127.0.0.1:8000/increase_brightness' \
     -H 'accept: application/json' \
     -d ''
     """
+    print("Increasing brightness")
 
-    increase_brightness_url = "/increase_brightness"
-    url = MAIN_URL + increase_brightness_url
+    url = MAIN_URL + INCREASE_BRIGHTNESS_URL
 
-    headers = {"accept": "application/json"}
-
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, headers=COMMON_HEADER)
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def decrease_brightness():
-    print("Decreasing brightness")
     """
     curl -X 'POST' \
     'http://127.0.0.1:8000/decrease_brightness' \
     -H 'accept: application/json' \
     -d ''
     """
+    print("Decreasing brightness")
 
-    decrease_brightness_url = "/decrease_brightness"
-    url = MAIN_URL + decrease_brightness_url
+    url = MAIN_URL + DECREASE_BRIGHTNESS_URL
 
-    headers = {"accept": "application/json"}
-
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, headers=COMMON_HEADER)
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def increase_color_temp():
-    print("Increasing color temperature")
     """
     curl -X 'POST' \
     'http://127.0.0.1:8000/increase_color_temp' \
     -H 'accept: application/json' \
     -d ''
     """
+    print("Increasing color temperature")
 
-    increase_color_temp_url = "/increase_color_temp"
-    url = MAIN_URL + increase_color_temp_url
+    url = MAIN_URL + INCREASE_COLOR_TEMP_URL
 
-    headers = {"accept": "application/json"}
-
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, headers=COMMON_HEADER)
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def decrease_color_temp():
-    print("Decreasing color temperature")
     """
     curl -X 'POST' \
     'http://127.0.0.1:8000/decrease_color_temp' \
     -H 'accept: application/json' \
     -d ''
     """
+    print("Decreasing color temperature")
 
-    decrease_color_temp_url = "/decrease_color_temp"
-    url = MAIN_URL + decrease_color_temp_url
+    url = MAIN_URL + DECREASE_COLOR_TEMP_URL
 
-    headers = {"accept": "application/json"}
-
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, headers=COMMON_HEADER)
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def get_brightness():
-    print("Getting brightness")
-
     """
     curl -X 'GET' \
     'http://127.0.0.1:8000/get_brightness' \
     -H 'accept: application/json'
     """
+    print("Getting brightness")
 
-    get_brightness_url = "/get_brightness"
-    url = MAIN_URL + get_brightness_url
+    url = MAIN_URL + GET_BRIGHTNRESS_URL
 
-    headers = {"accept": "application/json"}
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=COMMON_HEADER)
 
     print(response.status_code)
     print(response.text)
 
 @app.command()
 def get_color_temp():
-    print("Getting color temperature")
     """
     curl -X 'GET' \
     'http://127.0.0.1:8000/get_color_temp' \
     -H 'accept: application/json'
     """
+    print("Getting color temperature")
 
-    get_color_temp_url = "/get_color_temp"
-    url = MAIN_URL + get_color_temp_url
+    url = MAIN_URL + GET_COLOR_TEMP_URL
 
-    headers = {"accept": "application/json"}
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=COMMON_HEADER)
 
     print(response.status_code)
     print(response.text)
